3594228ef88051bc9046e4697a7b8521b3f0f588
nmdbn <lam2012004@gmail.com>
2024-12-22 14:49:41 +0700

    Update

--- KT/Controllers/DangKiesController.cs
+++ KT/Controllers/DangKiesController.cs
@@ -1,66 +1,4 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using KT.Models;
-//using System.Linq;
-
-//public class DangKiesController : Controller
-//{
-//    private readonly Test1Context _context;
-
-//    public DangKiesController(Test1Context context)
-//    {
-//        _context = context;
-//    }
-
-//    public IActionResult Index()
-//    {
-//        var maSV = User.Identity.Name;
-//        var registeredCourses = _context.DangKies
-//            .Where(d => d.MaSv == maSV)
-//            .SelectMany(d => d.ChiTietDangKies, (d, ctdk) => new
-//            {
-//                ctdk.MaHp,
-//                ctdk.HocPhan.TenHp,
-//                ctdk.HocPhan.SoTinChi
-//            })
-//            .ToList();
-
-//        return View(registeredCourses);
-//    }
-
-//    [HttpPost]
-//    public IActionResult Delete(string maHP)
-//    {
-//        var maSV = User.Identity.Name;
-//        var registration = _context.DangKies.FirstOrDefault(d => d.MaSv == maSV);
-//        if (registration != null)
-//        {
-//            var course = _context.ChiTietDangKies.FirstOrDefault(c => c.MaDk == registration.MaDk && c.MaHp == maHP);
-//            if (course != null)
-//            {
-//                _context.ChiTietDangKies.Remove(course);
-//                _context.SaveChanges();
-//            }
-//        }
-//        return RedirectToAction("Index");
-//    }
-
-//    [HttpPost]
-//    public IActionResult DeleteAll()
-//    {
-//        var maSV = User.Identity.Name;
-//        var registrations = _context.DangKies.Where(d => d.MaSv == maSV).ToList();
-//        foreach (var registration in registrations)
-//        {
-//            var courses = _context.ChiTietDangKies.Where(c => c.MaDk == registration.MaDk).ToList();
-//            _context.ChiTietDangKies.RemoveRange(courses);
-//        }
-//        _context.SaveChanges();
-
-//        return RedirectToAction("Index");
-//    }
-//}
-
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using KT.Models;
 using System.Linq;
 
@@ -124,7 +62,7 @@ public class DangKiesController : Controller
     [HttpPost]
     public IActionResult Save()
     {
-        // Thực hiện các bước lưu nếu cần thiết
+        TempData["SuccessMessage"] = "Thông tin đăng ký đã được lưu thành công.";
         return RedirectToAction("Index");
     }
 }
---
+++ KT/Models/DangKyViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KT.Models
+{
+    public class DangKyViewModel
+    {
+        public IEnumerable<dynamic> RegisteredCourses { get; set; }
+        public string MaSv { get; set; }
+        public string HoTen { get; set; }
+        public DateTime NgayDk { get; set; }
+        public DateTime? NgaySinh { get; set; } // Cập nhật kiểu dữ liệu thành DateTime?
+        public string NganhHoc { get; set; }
+        public int? MaDK { get; set; }
+    }
+}
